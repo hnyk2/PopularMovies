e55283c4af7ed0fc21e22d5d46113cb25c1c7f5a
hnyk2 <haniyekhanabadi12@gmail.com>
2020-06-28 00:45:08 +0430

    receive and show movie details.

--- app/src/main/java/com/example/popularmovies/MovieActivity.java
+++ app/src/main/java/com/example/popularmovies/MovieActivity.java
@@ -3,12 +3,31 @@ package com.example.popularmovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class MovieActivity extends AppCompatActivity {
+    ImageView imgBackdrop = findViewById(R.id.backdrop);
+    TextView tvTitle = findViewById(R.id.title);
+    TextView tvOverView = findViewById(R.id.overview);
+    TextView tvReleaseDate = findViewById(R.id.release);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_movie);
+        String backdrop = getIntent().getExtras().getString("movie-backdrop");
+        String title = getIntent().getExtras().getString("movie-title");
+        String overView = getIntent().getExtras().getString("movie-overview");
+        String releaseDate = getIntent().getExtras().getString("movie-release");
+
+
+        tvTitle.setText(title);
+        tvOverView.setText(overView);
+        tvReleaseDate.setText(releaseDate);
+        Glide.with(this).load(backdrop).into(imgBackdrop);
     }
 }
